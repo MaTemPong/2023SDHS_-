39829982f616357177ae33834aa7d65debb497fc
jonghyeok <061116pjh@naver.com>
2024-01-08 08:41:48 +0900

    FEAT: change import

--- wating/src/db/connect.js
+++ wating/src/db/connect.js
@@ -1,8 +1,8 @@
-import { connect } from 'mongoose';
+import mongoose from 'mongoose';
 
 const connectionString = `mongodb+srv://${process.env.REAT_APP_MONGODB_USERNAME}:${process.env.REAT_APP_MONGODB_PASSWORD}@sdhs.avssiuy.mongodb.net/?retryWrites=true&w=majority`
 
 export const dbConnect = async () => {
-  await connect(connectionString);
+  await mongoose.connect(connectionString);
   console.log('db connected');
 }
