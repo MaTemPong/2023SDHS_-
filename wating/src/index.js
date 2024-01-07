d49eda6544ddf974b8e03f5e0afce18ebb4751b0
pjhpkm0727 <061116pjh@naver.com>
2024-01-07 20:15:08 +0900

    FEAT: Modularization

--- wating/src/index.js
+++ wating/src/index.js
@@ -1,27 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/organisms/Header'
-
-import WaitManagement from './components/pages/WaitManagement'
-import PersonManagement from './components/pages/PersonManagement';
-import GroupManagement from './components/pages/GroupManagement';
+import Router from './components/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div className='App' style={{display:"flex"}}>
-		<BrowserRouter>
-        <Header/>
-		<Routes>
-			<Route path="/waitList" element={<WaitManagement />} />
-			<Route path="/personList" element={<PersonManagement />} />
-			<Route path="/groupList" element={<GroupManagement />} />
-		</Routes>
-		</BrowserRouter>
+  <div className='App' style={{display:"flex"}}>
+		<Router />
 	</div>
-  </React.StrictMode>
 );
-reportWebVitals();
