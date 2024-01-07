41a2279377dd1f75d4ccfe8c8b583b3f46a3fe0d
pjhpkm0727 <061116pjh@naver.com>
2024-01-07 16:55:14 +0900

    FEAT: set state

--- wating/src/components/organisms/Header/index.jsx
+++ wating/src/components/organisms/Header/index.jsx
@@ -1,33 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import * as S from './styled';
-import { Link } from "react-router-dom";
 import logo from '../../../media/logo.png'
 import MainList from "../../atoms/MainList";
 import AdminInfoView from "../../atoms/AdminInfoView"
 
 const Header = () => {
-    const urlList = [
-        {
-            url: '/waitList',
-            content: '웨이팅관리'
-        },
-        {
-            url: '/personList',
-            content: '인원관리'
-        },
-        {
-            url: '/groupList',
-            content: '조직관리'
+
+    const [wait, setWait] = useState(false);
+    const [person, setPerson] = useState(false);
+    const [group, setGroup] = useState(false);
+
+    const onClick = (props) => {
+        if(props === 'wait'){
+            setWait(true);
+            setPerson(false);
+            setGroup(false);
+        }else if(props === 'person'){
+            setWait(false);
+            setPerson(true);
+            setGroup(false);
+        }else{
+            setWait(false);
+            setPerson(false);
+            setGroup(true);
         }
-    ]
+    }
+
     return (
         <S.StyledHeader>
             <div>
                 <img src={logo} width={"100%"} style={{padding: "20px"}}/>
                 <S.Menu>
-                    <MainList link={'/waitList'} text={"웨이팅관리"}/>
-                    <MainList link={'/personList'} text={"인원관리"}/>
-                    <MainList link={'/groupList'} text={"조직관리"}/>
+                    <MainList state={wait} onClick={() => onClick('wait')} link={'/waitList'}>웨이팅관리</MainList>
+                    <MainList state={person} onClick={() => onClick('person')} link={'/personList'}>인원관리</MainList>
+                    <MainList state={group} onClick={() => onClick('group')} link={'/groupList'}>조직관리</MainList>
                 </S.Menu>
             </div>
             <AdminInfoView></AdminInfoView>
