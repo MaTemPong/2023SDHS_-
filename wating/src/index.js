d39f999ab484eb2e277781c5426afd8aa4acb265
pjhpkm0727 <061116pjh@naver.com>
2024-01-07 21:16:41 +0900

    FEAT: init setting

--- wating/src/index.js
+++ wating/src/index.js
@@ -1,6 +1,17 @@
 import './index.css';
 import ReactDOM from 'react-dom/client';
 import Router from './components/router';
+import { dbConnect } from './db/connect';
+
+require('dotenv').config();
+
+const init = async () => {
+
+	await dbConnect();
+
+}
+
+init();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
