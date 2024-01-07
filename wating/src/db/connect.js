cff4ce00391ab2b637b2f8aec4d62e2bb985a71a
jonghyeok <061116pjh@naver.com>
2024-01-08 08:32:00 +0900

    FEAT: react dotenv

--- wating/src/db/connect.js
+++ wating/src/db/connect.js
@@ -1,6 +1,6 @@
 import { connect } from 'mongoose';
 
-const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@sdhs.avssiuy.mongodb.net/?retryWrites=true&w=majority`
+const connectionString = `mongodb+srv://${process.env.REAT_APP_MONGODB_USERNAME}:${process.env.REAT_APP_MONGODB_PASSWORD}@sdhs.avssiuy.mongodb.net/?retryWrites=true&w=majority`
 
 export const dbConnect = async () => {
   await connect(connectionString);
