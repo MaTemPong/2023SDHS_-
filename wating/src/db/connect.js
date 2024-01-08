af5c736a5a11174e37d144cb6668197b76eb87c6
matempong <107092881+MaTemPong@users.noreply.github.com>
2024-01-08 10:41:45 +0900

    Revert "Merge branch 'main' of https://github.com/MaTemPong/2023SDHS_Hackathon"
    This reverts commit ca15887a158e8f4179d89adae36bc4d4a261278b, reversing
    changes made to acf113a63d9a15369e78646979b0ba34c510ed9b.

--- wating/src/db/connect.js
+++ wating/src/db/connect.js
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose';
+import { connect } from 'mongoose';
 
-const connectionString = `mongodb+srv://${process.env.REAT_APP_MONGODB_USERNAME}:${process.env.REAT_APP_MONGODB_PASSWORD}@sdhs.avssiuy.mongodb.net/?retryWrites=true&w=majority`
+const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@sdhs.avssiuy.mongodb.net/?retryWrites=true&w=majority`
 
 export const dbConnect = async () => {
-  await mongoose.connect(connectionString);
+  await connect(connectionString);
   console.log('db connected');
 }
