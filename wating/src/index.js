8ea7e59031791d246ecbefb97a116515271ae36f
matempong <107092881+MaTemPong@users.noreply.github.com>
2024-01-08 10:41:27 +0900

    Revert "Revert "...""
    This reverts commit 464a84670eda51a273cb11497acd5e2ad942f184.

--- wating/src/index.js
+++ wating/src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import ReactDOM from 'react-dom/client';
 import Router from './components/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
