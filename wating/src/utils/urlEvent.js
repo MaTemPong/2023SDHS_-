b1965f12a710f9095578b52392876d7a5f0c810c
jonghyeok <061116pjh@naver.com>
2024-01-08 09:45:17 +0900

    FEAT: create page object

--- wating/src/utils/urlEvent.js
+++ wating/src/utils/urlEvent.js
@@ -1,5 +1,20 @@
 const cur = '/' + window.location.href.split('/')[3];
 
+export const pageList = [
+    {
+        page: '/waitList',
+        isChecked: false,
+    },
+    {
+        page: '/personList',
+        isChecked: false,
+    },
+    {
+        page: '/groupList',
+        isChecked: false
+    }
+];
+
 export const MenuCheck = (link) => {
     if(cur === link) return '#0070b8'
     return '#004077';
