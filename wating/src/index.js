169dfc35e122c717bc978860320dd77092516c5c
matempong <107092881+MaTemPong@users.noreply.github.com>
2024-01-08 10:41:34 +0900

    Revert "Merge branch 'main' of https://github.com/MaTemPong/2023SDHS_Hackathon"
    This reverts commit 53b110f7b5d2c5220a7a4230af45a0f8859eb1c0, reversing
    changes made to 32ece1462ec74972c597269de11eecd437e8ac20.

--- wating/src/index.js
+++ wating/src/index.js
@@ -1,6 +1,20 @@
 import './index.css';
 import ReactDOM from 'react-dom/client';
 import Router from './components/router';
+import { dbConnect } from './db/connect';
+
+import 'dotenv/config';
+
+const init = async () => {
+
+	await dbConnect();
+
+}
+
+init()
+	.catch((err) => {
+		console.error('Error is occured while running application. Error: ', err);
+	});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
